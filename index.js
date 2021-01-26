96cec229de8bb3f93801a74a774fb6ae6f9dda91
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2021-01-25 20:43:06 -0600

    solution

---
+++ index.js
@@ -0,0 +1,20 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+  const reversedString = n.toString().split('').reverse().join('');
+  //   if (n < 0) {
+  //     return parseInt(reversedString) * -1;
+  //   }
+  //   return parseInt(reversedString);
+  return parseInt(reversedString) * Math.sign(n);
+}
+
+module.exports = reverseInt;
